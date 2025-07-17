6b054696efd9fb7a35a8159f67c1e6f9caf90b21
Tal Muskal <tal@musk.al>
2025-07-17 15:35:47 +0300

    Refactor script execution in CI/CD workflows to remove 'sh' prefix, enhancing consistency across build, test, and deploy scripts.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # install dependencies
-sh ./scripts/install.sh
+./scripts/install.sh
 
 # build
-
 # TODO: run build commands according to the project stack, requirements, etc. (for example: npm run build)
